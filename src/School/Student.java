8bbc48cf854f0c10caa31f27e0771a584df1f8e8
blackkolly <komolafe.solomon@yahoo.com>
2022-01-07 16:03:27 +0800

    Initial commit.Basic implementation of a string.

---
+++ src/School/Student.java
@@ -0,0 +1,9 @@
+package School;
+
+public class Student {
+    public static void main (String[] args) {
+        String name = "Oladipupo Kola";
+        System.out.println("I love this name " + name);
+
+    }
+}
