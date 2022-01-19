c4219c48d0e4a8bc18b2caff9f6386cfc61eac97
blackkolly <komolafe.solomon@yahoo.com>
2022-01-19 16:16:54 +0800

    Array practise

---
+++ src/MyArray/Practise.java
@@ -0,0 +1,14 @@
+package MyArray;
+
+public class Practise {
+    public static void main(String[] args){
+        System.out.println(Practise.addValue(3,4));
+        System.out.println(Practise.mutiplyValue(7,90));
+    }
+    public static int addValue(int x,int y){
+        return x + y;
+    }
+    public static int mutiplyValue(int x,int y) {
+        return x * y;
+    }
+}
