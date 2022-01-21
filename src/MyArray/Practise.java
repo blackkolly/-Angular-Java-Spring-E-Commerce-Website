e2e213320fd74e635faadb90d04ed76e31b09356
blackkolly <komolafe.solomon@yahoo.com>
2022-01-21 16:34:08 +0800

    Switch statement

--- src/MyArray/Practise.java
+++ src/MyArray/Practise.java
@@ -1,8 +1,10 @@
 package MyArray;
 
+import java.util.ArrayList;
+
 public class Practise {
     public static void main(String[] args){
-        System.out.println(Practise.addValue(3,4));
+     /**   System.out.println(Practise.addValue(3,4));
         System.out.println(Practise.mutiplyValue(7,90));
         System.out.println(Practise.divideValue(100,5));
     }
@@ -13,6 +15,32 @@ public class Practise {
         return x * y;
     } public static int divideValue(int x,int y) {
         return x / y;
+      */
+     ArrayList<String> myMonth = new ArrayList<String>();
+     int month = 8;
+     switch (month){
+         case 1: myMonth.add("January");
+         case 2: myMonth.add("Feburary");
+         case 3: myMonth.add("March");
+         case 4: myMonth.add("April");
+         case 5: myMonth.add("May");
+         case 6: myMonth.add("June");
+         case 7: myMonth.add("July");
+         case 8: myMonth.add("August");
+         case 9: myMonth.add("September");
+         case 10:myMonth.add("October");
+         case 11:myMonth.add("November");
+         break;
+     }
+     if(myMonth.isEmpty()){
+         System.out.println("Invalid month");
+        }
+     else{
+           for(String monthName: myMonth){
+               System.out.println(monthName);
+           }
+        }
+
     }
 
 }
